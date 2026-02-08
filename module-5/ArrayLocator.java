7dd024e2e39e12d20676991070b39c74a98fda55
alexander-hunt <alexanderthunt22@gmail.com>
2026-02-08 13:50:52 -0500

    5

---
+++ module-5/ArrayLocator.java
@@ -0,0 +1,70 @@
+public class ArrayLocator {
+
+    // -------- Largest (double) --------
+    public static int[] locateLargest(double[][] arrayParam) {
+        int[] location = new int[2];
+        double max = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] > max) {
+                    max = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    // -------- Largest (int) --------
+    public static int[] locateLargest(int[][] arrayParam) {
+        int[] location = new int[2];
+        int max = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] > max) {
+                    max = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    // -------- Smallest (double) --------
+    public static int[] locateSmallest(double[][] arrayParam) {
+        int[] location = new int[2];
+        double min = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] < min) {
+                    min = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+
+    // -------- Smallest (int) --------
+    public static int[] locateSmallest(int[][] arrayParam) {
+        int[] location = new int[2];
+        int min = arrayParam[0][0];
+
+        for (int i = 0; i < arrayParam.length; i++) {
+            for (int j = 0; j < arrayParam[i].length; j++) {
+                if (arrayParam[i][j] < min) {
+                    min = arrayParam[i][j];
+                    location[0] = i;
+                    location[1] = j;
+                }
+            }
+        }
+        return location;
+    }
+}
